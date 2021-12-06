e016e4c6e246c062b43ec9a5e44f2100edf727bf
Tenyo Hristov <tenyowork90@gmail.com>
2021-12-06 20:04:14 +0200

    Initial React

---
+++ TenyoTech.MealPreparation.FrontEnd.React/meal-preparation/src/App.js
@@ -0,0 +1,21 @@
+import { Route, Routes } from "react-router";
+import Header from "./components/Header/Header.js";
+import WelcomeWorld from "./components/WelcomeWorld/WelcomeWorld.js";
+
+function App() {
+  return (
+    <div>
+      <div id="box">
+        <Header/>
+
+        <main id="main-content">
+           <Routes>
+             <Route path="/" element={<WelcomeWorld/>}/>
+           </Routes>
+        </main>
+      </div>
+    </div>
+  );
+}
+
+export default App;
